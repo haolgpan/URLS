cccb695238da5620f7314e1a1c175a00d31cbffb
Hao <haolgpan@gmail.com>
2023-02-09 18:20:57 +0100

    Everything works

--- src/Survey/SurveyURLConn.java
+++ src/Survey/SurveyURLConn.java
@@ -6,7 +6,7 @@ import java.net.URLConnection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Test {
+public class SurveyURLConn {
     private static void fillForm(URL url, String formData) {
         URLConnection connection = null;
         try {
@@ -14,8 +14,10 @@ public class Test {
             connection.setDoOutput(true);
             OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
             out.write(formData);
+            System.out.println(connection.getIfModifiedSince());
             out.flush();
             out.close();
+            connection.getInputStream();
         } catch (Exception ex) {
             Logger.getLogger(MainSurvey.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -23,8 +25,8 @@ public class Test {
 
     public static void main(String[] args) {
         try {
-            String formData = "entry.835030737=Sad+life&entry.1616686619=No";
-            URL responseUrl = new URL("https://docs.google.com/forms/u/1/d/e/1FAIpQLSdV5QvhChK0fBpAMo5pN7sIvktqwHGu1vdoWJFvBguCeMvYUw/formResponse");
+            String formData = "entry.835030737=Sad+dreams&entry.1616686619=Si";
+            URL responseUrl = new URL("https://docs.google.com/forms/u/1/d/e/1FAIpQLSdV5QvhChK0fBpAMo5pN7sIvktqwHGu1vdoWJFvBguCeMvYUw/formResponse?");
 
             fillForm(responseUrl, formData);
         } catch (Exception e) {
